00283279ce1c80b4ea29e3fe0ae309267debcc00
Ngaicode <mjombakevin376@gmail.com>
2022-06-09 16:34:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+"use strict";
+const employee = {
+  name: "mwalo",
+  streetAddress: "Ngong road",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return Object.assign({}, employee, { [key]: value });
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const newObj = Object.assign({}, employee);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
